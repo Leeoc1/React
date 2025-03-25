813e91fa9bf527b8d44c676dfbef90c1d0eb91d7
Leeoc1 <lch5752@gmail.com>
2025-03-25 17:38:37 +0900

    ㄹ

---
+++ react-project/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// 예제 실습
+// import Library from "./chapter_3/Library";
+// import Clock from "./chapter_4/Clock";
+// import CommentList from "./chapter_5/CommentList";
+// import NotificationList from "./chapter_6/NotificationList";
+import Counter from "./chapter_7/Counter";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
